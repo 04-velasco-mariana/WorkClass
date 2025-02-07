4c4ab0494428ed9c9bf79b2bc4a8b67b11ffd607
04-velasco-mariana <22151240@aguascalientes.tecnm.mx>
2025-02-07 07:50:29 -0600

    Added first text interface in Main activity

--- app/src/main/java/Class/Variables.kt
+++ app/src/main/java/Class/Variables.kt
@@ -20,7 +20,7 @@ fun main(){
     val names = arrayOf("Erick", "Sofia", "Javier", "Veronica")
 
     println(age)
-    println("prueba push")
+    println("push") //prueba para hacer de nuevo el push porque el primero no funciono
     println("Welcome $name, to your first Kotlin project")
     println (add())
     println (product (5,8))
@@ -28,6 +28,13 @@ fun main(){
 
     val numbers = arrayOf(1,2,3,4,5,6,7,8,9,10)
     isEven (numbers)
+
+    println(getDay(1)) //Imprime funcion getDay
+
+    val person = Person ("Fernanda",21) //para la clase Person
+    person.displayInformation() //para mandar llamar la funcion con el metodo
+    println(person.name)
+    println(person.age)//imprimir solo un atributo
 }
 fun add() :Int{ //funcion añadir
     val x = 10
@@ -52,5 +59,25 @@ fun isEven(numbers:Array<Int>){
         }else {
             println("The number $number is odd")
         }
+    }
+}
+fun getDay(day:Int):String{
+    var name=""
+    when (day){
+        1 -> name = "Monday" //se hace la asignación en una sola línea de lo contrario se ponen llaves
+        2 -> name = "Tuesday"
+        3 -> name = "Wednesday"
+        4 -> name = "Thursday"
+        5 -> name = "Friday"
+        6 -> name = "Saturday"
+        7 -> name = "Sunday"
+        else -> name = "Incorrect value"
+    }
+    return name
+}
+class Person (val name:String, val age:Int){
+    fun displayInformation (){
+        println("Name: $name Age: $age")
+
     }
 }

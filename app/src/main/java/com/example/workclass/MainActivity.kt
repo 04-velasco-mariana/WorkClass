09f2b01ce377e4e94ee1fda27dbaace22af232b0
04-velasco-mariana <22151240@aguascalientes.tecnm.mx>
2025-02-12 07:51:58 -0600

    Box layouts and Text properties

--- app/src/main/java/com/example/workclass/MainActivity.kt
+++ app/src/main/java/com/example/workclass/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.workclass
 
 import android.os.Bundle
+import android.provider.CalendarContract.Colors
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -14,6 +15,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Scaffold
@@ -21,9 +23,16 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.workclass.ui.theme.WorkClassTheme
 
 class MainActivity : ComponentActivity() {
@@ -48,6 +57,8 @@ class MainActivity : ComponentActivity() {
                     }
                     Column() {
                         ModifierExample2()
+                        ModifierExample4()
+                        CustomText()
                     }
                 }
 
@@ -110,5 +121,49 @@ class MainActivity : ComponentActivity() {
             TextComposabe("4")
         }
     }
-}
+    @Preview(showBackground = true)
+    @Composable
+    fun ModifierExample4() {
+        Box(
+
+            modifier = Modifier
+                .background(Color.Red)
+                .padding(16.dp)
+                .width(300.dp)
+                .height(300.dp)
+        ) {
+            Text("1", Modifier.align(Alignment.TopStart))
+            Text("2", Modifier.align(Alignment.TopCenter))
+            Text("3", Modifier.align(Alignment.TopEnd))
+            Text("4", Modifier.align(Alignment.CenterStart))
+            Text("5", Modifier.align(Alignment.Center))
+            Text("6", Modifier.align(Alignment.CenterEnd))
+            Text("7", Modifier.align(Alignment.BottomStart))
+            Text("8", Modifier.align(Alignment.BottomCenter))
+            Text("9", Modifier.align(Alignment.BottomEnd))
+
+        }
+    }
+    @Preview(showBackground = true)
+    @Composable
+    fun CustomText() {
+        Column(){
+            Text(
+                stringResource(R.string.sample_text),
+                color = colorResource(R.color.purple_200),
+                fontSize = 20.sp,
+                fontStyle = FontStyle.Italic,
+                fontWeight = FontWeight.Bold,
+            )
+            val gradientColors = listOf(Color.Cyan, Color.Red, colorResource(R.color.purple_200))
+                Text(
+                stringResource(R.string.sample_text),
+                style = TextStyle(brush = Brush.linearGradient(colors=gradientColors ))
+
+            )
+        }
+    }
+
+
+} //close class
 

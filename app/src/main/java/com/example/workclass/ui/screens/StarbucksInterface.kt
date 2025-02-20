5129889d777b629bb0ebcdb1569441cc417b29cd
04-velasco-mariana <22151240@aguascalientes.tecnm.mx>
2025-02-19 21:46:40 -0600

    Commit for Rewards Section and Linear Progress Indicator

--- app/src/main/java/com/example/workclass/ui/screens/StarbucksInterface.kt
+++ app/src/main/java/com/example/workclass/ui/screens/StarbucksInterface.kt
@@ -42,7 +42,7 @@ fun StarbucksInterface(navController: NavController) {
             .verticalScroll(rememberScrollState())
     ) {
         TopBar()
-        //RewardSection()
+        RewardSection()
         //PromotionsList()
         //Spacer(modifier = Modifier.weight(1f))
         //BottomNavBar()
@@ -61,12 +61,63 @@ fun TopBar() { //barra de navegacion principal
             horizontalArrangement = Arrangement.SpaceBetween
         ) {
             Row {
-                Icon(Icons.Default.AccountBox, contentDescription = "Profile Icon", modifier = Modifier.size(30.dp)) //icono de Profile Icon
-                Spacer(modifier = Modifier.width(10.dp))
-                Icon(Icons.Default.Email, contentDescription = "Messages Icon", modifier = Modifier.size(30.dp)) //Icono de Email y mensajeria
+                Icon(Icons.Default.AccountBox, contentDescription = "Profile Icon",
+                    modifier = Modifier.size(30.dp)) //icono de Profile Icon
+                Spacer(modifier = Modifier.width(10.dp)) //Spacer se agrego para generar un espacio entre los iconos
+                Icon(Icons.Default.Email, contentDescription = "Messages Icon",
+                    modifier = Modifier.size(30.dp)) //Icono de Email y mensajeria
             }
-            Icon(Icons.Default.Settings, contentDescription = "Settings Icon", modifier = Modifier.size(30.dp)) // Icono de configuracion
+            Icon(Icons.Default.Settings, contentDescription = "Settings Icon",
+                modifier = Modifier.size(30.dp)) // Icono de configuracion
         }
     }
 }
+@Composable
+fun RewardSection() { //Seccion de la parte de Recompensas
+    Column(
+        modifier = Modifier.
+        padding(16.dp)) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Icon(
+                imageVector = Icons.Default.Star,
+                contentDescription = "Star Icon",
+                tint = colorResource(id = R.color.sacramento),
+                modifier = Modifier.
+                size(30.dp)
+            )
+            Spacer(
+                modifier = Modifier
+                    .width(5.dp))
+            Text("34 Star", fontSize = 24.sp, fontWeight = FontWeight.Bold)
+        }
+        Spacer(modifier = Modifier.height(8.dp))
+        LinearProgressIndicator( //Este es un elemento que agregué para poder visualizar la linea de progresion de las estrellas de recompensa
+            progress = 0.2f, //representa el porcentaje
+            modifier = Modifier.fillMaxWidth().height(8.dp)
+        )
+        Spacer(modifier = Modifier.height(10.dp))
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.End
+        ) {
+            OutlinedButton(onClick = { /* no tiene acciones el boton */ }, shape = RoundedCornerShape(20.dp)) { //use este tipo de boton que solo tiene el margen mas no esta relleno
+                Row(verticalAlignment = Alignment.CenterVertically) {
+                    Text("Rewards Details")
+                    Spacer(modifier = Modifier.width(5.dp))
+                    Icon(
+                        imageVector = Icons.Default.Star,
+                        contentDescription = "Star Icon",
+                        tint = colorResource(id = R.color.sacramento), //mando a llamar un color que cree en xml colors
+                        modifier = Modifier.size(20.dp)
+                    )
+                }
+            }
+        }
+    }
+}
+
+
+
 

28d9e2b552f83dfa86d89ac2da29b945c8e73bed
04-velasco-mariana <22151240@aguascalientes.tecnm.mx>
2025-04-07 07:53:14 -0600

    Commit whit dependencies of Internal Storage with Room, and creation of DataBase Files

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    id("com.google.devtools.ksp")
 }
 
 android {
@@ -83,6 +84,10 @@ dependencies {
     //
     //implementation ("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
     implementation (libs.androidx.lifecycle.runtime.ktx.v262)
+    //
+    val room_version = "2.6.1"
+
+    implementation("androidx.room:room-runtime:$room_version")
 
 
 
--- app/src/main/java/com/example/workclass/data/model/AccountModel.kt
+++ app/src/main/java/com/example/workclass/data/model/AccountModel.kt
@@ -1,6 +1,10 @@
 package com.example.workclass.data.model
 
 import android.content.ClipDescription
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import kotlin.contracts.Returns
 
 data class AccountModel(
     var id : Int =0,
@@ -9,5 +13,25 @@ data class AccountModel(
     var password:String ="",
     var description: String ="",
     var imageURL: String = ""
-
 )
+
+@Entity
+data class AccountEntity(
+    @PrimaryKey val id:Int,
+    @ColumnInfo (name = "name") val name: String,
+    @ColumnInfo (name = "username") val username: String,
+    @ColumnInfo (name = "password") val password: String,
+    @ColumnInfo (name = "description") val description: String,
+    @ColumnInfo (name = "imageURL") val imageURL: String
+)
+
+fun AccountModel.toAccountEntity(): AccountEntity{
+    return AccountEntity(
+        id = this.id,
+        name = this.name,
+        username= this.username,
+        password= this.password,
+        description= this.description,
+        imageURL = this.imageURL
+    )
+}
